67c35c92d52569776445d8beb97e6436aca3bc44
Joelson Alves de Melo Junior <joelsonalves@localhost.localdomain>
2018-08-22 06:47:59 -0300

    ajustes na home

--- public_html/resultadorapido/index.php
+++ public_html/resultadorapido/index.php
@@ -1,3 +1,7 @@
+<?php
+	session_start(); // Inicializa.
+	session_unset(); // Remove todas as variáveis.
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,61 +15,58 @@
 </head>
 <body>
 	<!-- Cabeçalho -->
-	<header class="jumbotron bg-light" style="margin-bottom:0;">
-		<h1>Resultado Rápido</h1>
-		<p>Sistema de Apoio as Avaliações Escolares</p>
+	<header class="jumbotron bg-light" style="margin-bottom:0; padding: 20pt;">
+		<div class="row col-sm-12">
+		<div style="padding-right: 20pt;">
+			<img class="img-fluid img-thumbnail" src="../complementos/img/brasao_pe.png" alt="Brasão de Pernambuco" style="width: 100pt; height: auto;">
+		</div>
+		<div style="padding-top: 20pt;">
+			<h1>Resultado Rápido</h1>
+			<p>Sistema de Apoio das Avaliações Bimestrais da EESJ</p>
+		</div>
+		</div>
 	</header>
 	<!-- Barra de Menus -->
 	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
 		<!-- Links -->
 		<ul class="navbar-nav">
-			<li class="nav-item">
-				<a class="nav-link" href="#">Início</a>
+			<li class="nav-item active">
+				<a class="nav-link" href="/resultadorapido">Início</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="#">Resultado</a>
+				<a class="nav-link" href="./estudante.html">Estudante</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="#">Gabarito</a>
+				<a class="nav-link" href="./docente.html">Docente</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="#">Administrador</a>
+				<a class="nav-link" href="./admin.html">Administração</a>
 			</li>
 		</ul>
 	</nav>
-	<section class="jumbotron bg-light">
-		<h4>Lançamento de Marcações</h4>
-		<form action="" style="margin-top:20px">
-			<div class="form-group">
-				<label for="matricula">Digite sua <strong>Matrícula</strong></label>
-				<input type="text" class="form-control" id="matricula"/>
+	<section class="jumbotron bg-light" style="padding: 0; margin: 0; border: 0;">
+		<div id="slideshow" class="carousel slide" data-ride="carousel">
+			<ul class="carousel-indicators">
+				<li data-target="estudante" data-slide-to="0" class="active"></li>
+				<li data-target="docente" data-slide-to="1"></li>
+			</ul>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img src="../complementos/img/banner_estudante.png" alt="Estudante" width="100%" height="auto">
+					<div class="carousel-caption bg-dark" style="padding: 10pt; opacity: 0.9;">
+						<h3><strong>Estudante</strong>,</h3>
+						<p>Esse sistema lhe permitirá lançar suas marcações no <strong>cartão-resposta</strong> virtual e, conhecer seu desempenho na <strong>avaliação</strong> realizada.</p>
+					</div> 
+				</div>
+				<div class="carousel-item">
+					<img src="../complementos/img/banner_docente.png" alt="Docente" width="100%" height="auto">
+					<div class="carousel-caption bg-dark" style="padding: 10pt; opacity: 0.9">
+						<h3><strong>Docente</strong>,</h3>
+						<p>Esse sistema lhe permitirá definir o <strong>gabarito</strong>, autorizar o lançamento do  <strong>cartão-resposta</strong> pelo estudante e, conhecer o desempenho da turma na <strong>avaliação</strong> em curso.</p>
+					</div>   
+				</div>
 			</div>
-			<div class="form-group">	
-				<label for="nome">Confira seu <strong>Nome</strong></label>
-				<input type="text" class="form-control" id="nome" readonly/>
-			</div>
-			<div class="form-group">	
-				<label for="data_nascimento">Confira sua <strong>Data de Nascimento</strong></label>
-				<input type="date" class="form-control" id="data_nascimento" readonly/>
-			</div>
-			<div class="form-group">
-  				<label for="turma">Confira sua <strong>Turma</strong></label>
-  				<input type="text" class="form-control" id="turma" readonly/>
-			</div>
-			<div class="form-group">
-  				<label for="bimestre">Confira o <strong>Bimestre</strong></label>
-  				<input type="text" class="form-control" id="bimestre" readonly/>
-			</div>
-			<div class="form-group">
-  				<label for="dia_prova">Selecione o <strong>Dia</strong></label>
-  				<select class="form-control" id="dia_prova">
-    				<option>1º Dia</option>
-    				<option>2º Dia</option>
-    				<option>3º Dia</option>
-    				<option>4º Dia</option>
-  				</select>
-			</div>
-		</form>
+		</div>
 	</section>
 </body>
 </html>
